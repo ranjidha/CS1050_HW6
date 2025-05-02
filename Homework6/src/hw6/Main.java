cad714e81f00b3af15b37f142a381b070477cf2b
rranjidh <rranjidh@192.168.0.133>
2025-05-01 21:22:44 -0600

    RR_Commit1

---
+++ Homework6/src/hw6/Main.java
@@ -0,0 +1,23 @@
+package hw6;
+//Name
+//Time taken:
+
+//Add other classes here or separate java files in same package
+public class Main {
+
+	public static void main(String[] args) {
+				
+		/* Add your Menu Here
+		 * 1. Add Book
+		   2. Remove Book
+		   3. Display All Books
+		   4. Search by Title
+		   5. Search by Author
+		   6. Check Out Book
+		   7. Return Book
+		   8. Exit
+
+		 */
+	}
+
+}
